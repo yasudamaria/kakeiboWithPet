e631c5ef179cf65a1295b17373250f15c1d96318
yasudamaria <hibarihaku2001@gmail.com>
2023-10-26 21:41:48 +0900

    カテゴリ追加(健康・医療まで)

--- imports/ui/Category.jsx
+++ imports/ui/Category.jsx
@@ -5,6 +5,313 @@ export const Category = () => {
     return (
         <div class="container">
             <p>カテゴリー</p>
+
+            <div class="container bg-light m-1">
+                
+                {/* 食費 */}
+                <div class="category">
+                    <div class="big_category row">
+                        <div class="col">
+                            <h5>🍚食費</h5>
+                        </div>
+                        <div class="col">
+                            <h5>10000円</h5>
+                        </div>
+                    </div>
+
+                    <div class="smoll_category_container">
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>🥬食料品</p>
+                            </div>
+                            <div class="col">
+                                <p>5000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>🍩外食</p>
+                            </div>
+                            <div class="col">
+                                <p>2000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>☕カフェ</p>
+                            </div>
+                            <div class="col">
+                                <p>3000円</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                {/* 交通費 */}
+                <div class="category">
+                    <div class="big_category row">
+                        <div class="col">
+                            <h5>🚄交通費</h5>
+                        </div>
+                        <div class="col">
+                            <h5>10000円</h5>
+                        </div>
+                    </div>
+
+                    <div class="smoll_category_container">
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>🚃電車</p>
+                            </div>
+                            <div class="col">
+                                <p>5000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>🚌バス</p>
+                            </div>
+                            <div class="col">
+                                <p>2000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>🚙車</p>
+                            </div>
+                            <div class="col">
+                                <p>3000円</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            
+                {/* 日用品 */}
+                <div class="category">
+                    <div class="big_category row">
+                        <div class="col">
+                            <h5>🧹日用品</h5>
+                        </div>
+                        <div class="col">
+                            <h5>10000円</h5>
+                        </div>
+                    </div>
+
+                    <div class="smoll_category_container">
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>🧻日用品</p>
+                            </div>
+                            <div class="col">
+                                <p>5000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>🐼ペット</p>
+                            </div>
+                            <div class="col">
+                                <p>2000円</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                {/* 趣味 */}
+                <div class="category">
+                    <div class="big_category row">
+                        <div class="col">
+                            <h5>✨趣味</h5>
+                        </div>
+                        <div class="col">
+                            <h5>10000円</h5>
+                        </div>
+                    </div>
+
+                    <div class="smoll_category_container">
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>⛺アウトドア</p>
+                            </div>
+                            <div class="col">
+                                <p>5000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>🎬映画・音楽・ゲーム</p>
+                            </div>
+                            <div class="col">
+                                <p>2000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>📚本</p>
+                            </div>
+                            <div class="col">
+                                <p>2000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>🛩旅行</p>
+                            </div>
+                            <div class="col">
+                                <p>2000円</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                {/* 交際費 */}
+                <div class="category">
+                    <div class="big_category row">
+                        <div class="col">
+                            <h5>👤交際費</h5>
+                        </div>
+                        <div class="col">
+                            <h5>10000円</h5>
+                        </div>
+                    </div>
+
+                    <div class="smoll_category_container">
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>🍖ご飯</p>
+                            </div>
+                            <div class="col">
+                                <p>5000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>⛲交際費</p>
+                            </div>
+                            <div class="col">
+                                <p>2000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>🎁プレゼント</p>
+                            </div>
+                            <div class="col">
+                                <p>2000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>💒冠婚葬祭</p>
+                            </div>
+                            <div class="col">
+                                <p>2000円</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                {/* 衣服・美容 */}
+                <div class="category">
+                    <div class="big_category row">
+                        <div class="col">
+                            <h5>👚衣服・美容</h5>
+                        </div>
+                        <div class="col">
+                            <h5>10000円</h5>
+                        </div>
+                    </div>
+
+                    <div class="smoll_category_container">
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>👖衣服</p>
+                            </div>
+                            <div class="col">
+                                <p>5000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>✂美容院</p>
+                            </div>
+                            <div class="col">
+                                <p>2000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>💄化粧品</p>
+                            </div>
+                            <div class="col">
+                                <p>2000円</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                {/* 健康・医療 */}
+                <div class="category">
+                    <div class="big_category row">
+                        <div class="col">
+                            <h5>✚健康・医療</h5>
+                        </div>
+                        <div class="col">
+                            <h5>10000円</h5>
+                        </div>
+                    </div>
+
+                    <div class="smoll_category_container">
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>👏ボディケア</p>
+                            </div>
+                            <div class="col">
+                                <p>5000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">👏
+                                <p>🏥医療費</p>
+                            </div>
+                            <div class="col">
+                                <p>2000円</p>
+                            </div>
+                        </div>
+
+                        <div class="smoll_category row">
+                            <div class="col">
+                                <p>💊薬</p>
+                            </div>
+                            <div class="col">
+                                <p>2000円</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
